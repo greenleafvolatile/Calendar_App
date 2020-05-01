1656fe36e311caea68c4e6fc8056b66217ce5f2e
greenleafvolatile <greenleafvolatile@users.noreply.github.com>
2020-05-01 12:02:03 +0200

    Added Event.java

---
+++ src/Event.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class Event {
+
+    private LocalDateTime startDateAndTime, endDateAndTime;
+    private String name;
+    private int id;
+
+    public Event(){
+
+
+    }
+}
